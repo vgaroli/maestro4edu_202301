4b38ee2952d2212f2705914619998ace53d38217
Valter Garoli <vgaroli@gmail.com>
2023-02-16 16:15:17 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'maestro4edu_202301';
+}
